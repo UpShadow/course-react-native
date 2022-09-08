0820b68c2d3b5b744dd3f7dba720c490c8ee5c83
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-09-08 00:25:33 -0300

    Passo 1

--- exercises/App.js
+++ exercises/App.js
@@ -75,7 +75,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Passo #02">
+          <Section title="Passo #01">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
